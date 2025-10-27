1a27d655a3e1c666556dd55f281003bf41507e27
albanmuhammad <albanmuhammad07@gmail.com>
2025-10-27 15:19:20 +0700

    test koneksi mc 3

---
+++ src/app/api/friends/route.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  return NextResponse.json({ ok: true, id: params.id });
+}
